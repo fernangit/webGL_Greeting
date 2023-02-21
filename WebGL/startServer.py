dc90f0a6777a086fba7a4e33b7e77d69ad2d3ebc
fernangit <morio.tsunaki@gmail.com>
2023-02-21 21:47:08 +0900

    modify

--- WebGL/startServer.py
+++ WebGL/startServer.py
@@ -1,6 +1,6 @@
 #https://aiacademy.jp/media/?p=57
 #http://localhost:8000/
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__, static_folder='.', static_url_path='')
 
@@ -9,7 +9,7 @@ def index():
     return app.send_static_file('index.html')
 
 @app.route('/StreamingAssets/Utterance', methods=['POST'])
-def updateScore():
+def updateUtter():
     print('post');
     
     filename = 'StreamingAssets/Utterance/utter.txt'
